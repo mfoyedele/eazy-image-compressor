3195fe42eeb43d83d61bcb51411925f36eb1ee59
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-08-24 19:57:43 +0100

    Update jtw-middleware

--- helpers/api/jwt-middleware.js
+++ helpers/api/jwt-middleware.js
@@ -1,22 +1,22 @@
-import expressJwt from 'express-jwt';
-import { promisify } from 'util';
+const { expressjwt: expressJwt } = require('express-jwt');
+const util = require('util');
 import getConfig from 'next/config';
 
 const { serverRuntimeConfig } = getConfig();
 
-function jwtMiddleware(req, res) {
-    const middleware = expressJwt({
-        secret: serverRuntimeConfig.secret,
-        algorithms: ['HS256']
-    }).unless({
-        path: [
-            // public routes that don't require authentication
-            '/api/users/register',
-            '/api/users/authenticate'
-        ]
-    });
-
-    return promisify(middleware)(req, res);
-}
-
 export { jwtMiddleware };
+
+function jwtMiddleware(req, res) {
+  const middleware = expressJwt({
+    secret: serverRuntimeConfig.secret,
+    algorithms: ['HS256'],
+  }).unless({
+    path: [
+      // public routes that don't require authentication
+      '/api/users/register',
+      '/api/users/authenticate',
+    ],
+  });
+
+  return util.promisify(middleware)(req, res);
+}
