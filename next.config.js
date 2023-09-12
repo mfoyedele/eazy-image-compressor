13979ed86d5aca3b2227f4a121171755061e813a
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-09-12 09:47:18 +0100

    Update nav

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
 
-module.exports = {
-    reactStrictMode: true,
-    serverRuntimeConfig: {
-        connectionString: "mongodb+srv://image_compressor:alximage123@cluster0.msasavb.mongodb.net/?retryWrites=true",
-        secret: 'MY NAME IS OYEDELE MUSA FUNSO'
-    },
-    publicRuntimeConfig: {
-        apiUrl: process.env.NODE_ENV === 'development'
-            ? 'https://funso-image-compressor.vercel.app/api' // development api
-            : 'https://funso-image-compressor.vercel.app/api' // production api
-    }
-}
-
-
-
 // module.exports = {
 //     reactStrictMode: true,
 //     serverRuntimeConfig: {
@@ -22,7 +7,22 @@ module.exports = {
 //     },
 //     publicRuntimeConfig: {
 //         apiUrl: process.env.NODE_ENV === 'development'
-//             ? 'http://localhost:3000/api' // development api
-//             : 'http://localhost:3000/api' // production api
+//             ? 'https://funso-image-compressor.vercel.app/api' // development api
+//             : 'https://funso-image-compressor.vercel.app/api' // production api
 //     }
 // }
+
+
+
+module.exports = {
+    reactStrictMode: true,
+    serverRuntimeConfig: {
+        connectionString: "mongodb+srv://image_compressor:alximage123@cluster0.msasavb.mongodb.net/?retryWrites=true",
+        secret: 'MY NAME IS OYEDELE MUSA FUNSO'
+    },
+    publicRuntimeConfig: {
+        apiUrl: process.env.NODE_ENV === 'development'
+            ? 'http://localhost:3000/api' // development api
+            : 'http://localhost:3000/api' // production api
+    }
+}
